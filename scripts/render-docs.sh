5de6893d59df73ad206f7a0cdd337b7dedc19927
cocoa <cocoa@mintcocoa.cc>
2026-05-10 06:15:35 +0000

    Split runtime module portfolio docs

--- scripts/render-docs.sh
+++ scripts/render-docs.sh
@@ -6,5 +6,8 @@ cd "$repo_root"
 
 quarto render docs/index.qmd --to html
 quarto render docs/server/ServerCorePortfolio.qmd --to html
+quarto render docs/server/RuntimeWebPortfolio.qmd --to html
+quarto render docs/server/RuntimeProxyPortfolio.qmd --to html
+quarto render docs/server/RuntimeGamePortfolio.qmd --to html
 quarto render docs/client/ClientPortfolio.qmd --to html
 "$repo_root/scripts/render-devops.sh"
